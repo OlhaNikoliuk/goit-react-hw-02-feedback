315accbdbb72e1d74be62db22bf8ee425b708fc9
OlhaNikoliuk <nikoliukolha@gmail.com>
2021-07-06 12:44:23 +0300

    add sol. for hw-feedback

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+import Container from './components/Container/Container';
+import Statistics from './components/Statistics/Statistics';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Section from './components/Section/Section';
+import Notification from './components/Notification/Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = option => {
+    this.setState(prevState => {
+      return { [option]: prevState[option] + 1 };
+    });
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const totalVoutes = Object.values(this.state).reduce(
+      (sum, value) => sum + value,
+    );
+
+    return Math.round(`${(this.state.good * 100) / totalVoutes}`);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    const options = Object.keys(this.state);
+
+    return (
+      <Container>
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
+          ></FeedbackOptions>
+        </Section>
+
+        <Section title={'Statistic'}>
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            ></Statistics>
+          ) : (
+            <Notification message={'No feedback given'} />
+          )}
+        </Section>
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Container/Container.styled.js
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled/macro';
+
+export const Wrapper = styled.div`
+  width: 1024px;
+  padding: 30px 15px;
+  /* margin: 0 auto; */
+`;
